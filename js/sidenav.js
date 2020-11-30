f47ca23ead17f7bcb695659d98769c4ff2db6f14
vitor <vitorss>
2020-11-30 09:26:13 -0300

    Nova atualização

---
+++ js/sidenav.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("openSidenav").style.width = "23%";
+    document.getElementById("screen").style.marginLeft = "23%";
+}
+
+function closeNav() {
+    document.getElementById("openSidenav").style.width = "0";
+    document.getElementById("screen").style.marginLeft = "0";
+}
